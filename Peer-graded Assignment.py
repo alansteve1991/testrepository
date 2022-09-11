1b899141dca7b897175a767e11f0d516db211309
alansteve1991 <112873784+alansteve1991@users.noreply.github.com>
2022-09-11 14:23:51 +0100

    Add files via upload

---
+++ Peer-graded Assignment.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # H1 "My Jupyter Notebook on IBM Watson Studio"
+
+# **SOMRUTHAI** **LAMJIEKROONGRUANG**
+# I would like to be a data scientist
+
+# _I am interested in data science because..._
+# 
+# _Data scientist is the sexiest job_
+
+# ##### Python is the code eg.
+
+# In[1]:
+
+
+1+1
+
+
+# + Dot
+# Inline-style: 
+# ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+# 
+# Markdown | Less | Pretty
+# --- | --- | ---
+# *Still* | `renders` | **nicely**
+# 1 | 2 | 3
+
+# In[ ]:
+
+
+
+
